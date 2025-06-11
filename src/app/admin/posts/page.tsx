c0107ea30c1b22fdd6e1335ba1afaefa0f06ddfc
Jihan4105 <wlgks6979@naver.com>
2025-06-11 20:38:33 +0900

    2025-06-11 Developments

--- src/app/admin/posts/page.tsx
+++ src/app/admin/posts/page.tsx
@@ -29,6 +29,7 @@ import { LuDot } from "react-icons/lu";
 export default function PostsManagementPage() {
   const [isSearchEnabled, setIsSearchEnabled] = useState<boolean>(false);
   const [searchFilter, setSearchFilter] = useState<string>("title");
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
   return (
     <div className="mt-3">
@@ -43,16 +44,16 @@ export default function PostsManagementPage() {
             <div>
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                  <Button variant={"ghost"} className="focus-visible:border-0">
+                  <Button variant={"ghost"} className="focus-visible:border-0 p-0! mr-2">
                     QuickSearch
                     <IoChevronDown />
                   </Button>
                 </DropdownMenuTrigger>
-                <DropdownMenuContent className="bg-white">
+                <DropdownMenuContent className="bg-white border-[#d2d2d2]">
                   <DropdownMenuItem>
                     View all posts
                   </DropdownMenuItem>
-                  <DropdownMenuSeparator className="bg-black"/>
+                  <DropdownMenuSeparator className="bg-gray-200"/>
                   <DropdownMenuLabel className="text-gray-400">Status view</DropdownMenuLabel>
                   <DropdownMenuItem>
                     <span className="ml-3">
@@ -69,7 +70,7 @@ export default function PostsManagementPage() {
                       private
                     </span>
                   </DropdownMenuItem>
-                  <DropdownMenuSeparator className="bg-black" />
+                  <DropdownMenuSeparator className="bg-gray-200" />
                   <DropdownMenuLabel className="text-gray-400">Category view</DropdownMenuLabel>
                   <DropdownMenuItem>
                     <span className="ml-3">
@@ -98,7 +99,11 @@ export default function PostsManagementPage() {
                   </DropdownMenuItem>
                 </DropdownMenuContent>
               </DropdownMenu>
-              <Button variant={"ghost"} onClick={() => setIsSearchEnabled(!isSearchEnabled)}>
+              <Button 
+                variant={"ghost"} 
+                onClick={() => setIsSearchEnabled(!isSearchEnabled)}
+                className="p-0!"
+              >
                 Search
                 <IoSearch />
               </Button>
@@ -107,10 +112,10 @@ export default function PostsManagementPage() {
         : 
           <>
             <DropdownMenu>
-              <DropdownMenuTrigger asChild className="border-[#d2d2d2]">
-                <Button variant="outline" className="text-gray-600">{searchFilter}</Button>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="p-0! mr-2">{searchFilter} <IoChevronDown /></Button>
               </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56 border-[#d2d2d2] bg-white text-gray-600" align="start">
+              <DropdownMenuContent className="min-w-[60px] border-[#d2d2d2] bg-white" align="start">
                 <DropdownMenuItem
                   onSelect={() => setSearchFilter("title")}
                   className="hover:bg-gray-100"
@@ -130,8 +135,11 @@ export default function PostsManagementPage() {
               id="search" 
               name="search" 
               type="text" 
-              className="outline-none p-3 border-b border-[#d2d2d2] w-full mb-5 text-[25px]"
+              className="outline-none border-[#d2d2d2] w-full"
               placeholder="Search here"
+              autoCorrect="off"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
             />
             <Button variant={"ghost"} onClick={() => setIsSearchEnabled(!isSearchEnabled)}>
               close
@@ -139,19 +147,21 @@ export default function PostsManagementPage() {
           </>
         }
       </div>
-      <ul>
-        <li className="group">
-          <div>
-            <Checkbox />
+      <ul className="flex flex-col border-1 border-gray-200 mt-3">
+        <li className="group py-5 px-4 flex items-center hover:bg-[#F9F9FF] cursor-pointer justify-between [&:not(:last-child)]:border-b border-gray-200">
+          <div className="flex items-center">
+            <Checkbox className="data-[state=checked]:bg-black data-[state=checked]:text-white size-[20px] mr-4"/>
             <div>
               Title of the post
-              <div>
-                <span>IT/NextJS</span>
-                <LuDot />
+              <div className="flex items-center text-gray-400 text-xs">
+                <span className="text-[#7979FF] mr-1">IT/NextJS</span>
+                <LuDot className="mr-1"/>
                 <span>2025-04-05 22:18</span>
               </div>
             </div>
-            <div className="hidden group-hover:flex">
+          </div>
+          <div className="sm:justify-self-end group-hover:flex group-hover:visible">
+            <div className="hidden lg:flex gap-2">
               <Button variant={"destructive"}>edit</Button>
               <Button variant={"outline"}>delete</Button>
               <DropdownMenu>
@@ -167,6 +177,280 @@ export default function PostsManagementPage() {
                   <DropdownMenuItem
                     className="hover:bg-gray-100"
                   >
+                    Private
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            <div className="lg:hidden">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="ghost" className="ml-4 ">
+                    <span className="sr-only">Actions</span>
+                    ⋮
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="min-w-[120px] border-[#d2d2d2] bg-white text-gray-600">
+                  <DropdownMenuItem>
+                    edit
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    delete
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuLabel>Status</DropdownMenuLabel>
+                  <DropdownMenuItem>
+                    Public
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    Private
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+        </li>
+        <li className="group py-5 px-4 flex items-center hover:bg-[#F9F9FF] cursor-pointer justify-between [&:not(:last-child)]:border-b border-gray-200">
+          <div className="flex items-center">
+            <Checkbox className="data-[state=checked]:bg-black data-[state=checked]:text-white size-[20px] mr-4"/>
+            <div>
+              Title of the post
+              <div className="flex items-center text-gray-400 text-xs">
+                <span className="text-[#7979FF] mr-1">IT/NextJS</span>
+                <LuDot className="mr-1"/>
+                <span>2025-04-05 22:18</span>
+              </div>
+            </div>
+          </div>
+          <div className="sm:justify-self-end group-hover:flex group-hover:visible">
+            <div className="hidden lg:flex gap-2">
+              <Button variant={"destructive"}>edit</Button>
+              <Button variant={"outline"}>delete</Button>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild className="border-[#d2d2d2]">
+                  <Button variant="outline" className="text-gray-600">Public</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="min-w-[80px] border-[#d2d2d2] bg-white text-gray-600" side="right">
+                  <DropdownMenuItem
+                    className="hover:bg-gray-100"
+                  >
+                    Publice
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="hover:bg-gray-100"
+                  >
+                    Private
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            <div className="lg:hidden">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="ghost" className="ml-4 ">
+                    <span className="sr-only">Actions</span>
+                    ⋮
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="min-w-[120px] border-[#d2d2d2] bg-white text-gray-600">
+                  <DropdownMenuItem>
+                    edit
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    delete
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuLabel>Status</DropdownMenuLabel>
+                  <DropdownMenuItem>
+                    Public
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    Private
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+        </li>
+        <li className="group py-5 px-4 flex items-center hover:bg-[#F9F9FF] cursor-pointer justify-between [&:not(:last-child)]:border-b border-gray-200">
+          <div className="flex items-center">
+            <Checkbox className="data-[state=checked]:bg-black data-[state=checked]:text-white size-[20px] mr-4"/>
+            <div>
+              Title of the post
+              <div className="flex items-center text-gray-400 text-xs">
+                <span className="text-[#7979FF] mr-1">IT/NextJS</span>
+                <LuDot className="mr-1"/>
+                <span>2025-04-05 22:18</span>
+              </div>
+            </div>
+          </div>
+          <div className="sm:justify-self-end group-hover:flex group-hover:visible">
+            <div className="hidden lg:flex gap-2">
+              <Button variant={"destructive"}>edit</Button>
+              <Button variant={"outline"}>delete</Button>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild className="border-[#d2d2d2]">
+                  <Button variant="outline" className="text-gray-600">Public</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="min-w-[80px] border-[#d2d2d2] bg-white text-gray-600" side="right">
+                  <DropdownMenuItem
+                    className="hover:bg-gray-100"
+                  >
+                    Publice
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="hover:bg-gray-100"
+                  >
+                    Private
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            <div className="lg:hidden">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="ghost" className="ml-4 ">
+                    <span className="sr-only">Actions</span>
+                    ⋮
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="min-w-[120px] border-[#d2d2d2] bg-white text-gray-600">
+                  <DropdownMenuItem>
+                    edit
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    delete
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuLabel>Status</DropdownMenuLabel>
+                  <DropdownMenuItem>
+                    Public
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    Private
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+        </li>
+        <li className="group py-5 px-4 flex items-center hover:bg-[#F9F9FF] cursor-pointer justify-between [&:not(:last-child)]:border-b border-gray-200">
+          <div className="flex items-center">
+            <Checkbox className="data-[state=checked]:bg-black data-[state=checked]:text-white size-[20px] mr-4"/>
+            <div>
+              Title of the post
+              <div className="flex items-center text-gray-400 text-xs">
+                <span className="text-[#7979FF] mr-1">IT/NextJS</span>
+                <LuDot className="mr-1"/>
+                <span>2025-04-05 22:18</span>
+              </div>
+            </div>
+          </div>
+          <div className="sm:justify-self-end group-hover:flex group-hover:visible">
+            <div className="hidden lg:flex gap-2">
+              <Button variant={"destructive"}>edit</Button>
+              <Button variant={"outline"}>delete</Button>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild className="border-[#d2d2d2]">
+                  <Button variant="outline" className="text-gray-600">Public</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="min-w-[80px] border-[#d2d2d2] bg-white text-gray-600" side="right">
+                  <DropdownMenuItem
+                    className="hover:bg-gray-100"
+                  >
+                    Publice
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="hover:bg-gray-100"
+                  >
+                    Private
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            <div className="lg:hidden">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="ghost" className="ml-4 ">
+                    <span className="sr-only">Actions</span>
+                    ⋮
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="min-w-[120px] border-[#d2d2d2] bg-white text-gray-600">
+                  <DropdownMenuItem>
+                    edit
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    delete
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuLabel>Status</DropdownMenuLabel>
+                  <DropdownMenuItem>
+                    Public
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    Private
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+        </li>
+        <li className="group py-5 px-4 flex items-center hover:bg-[#F9F9FF] cursor-pointer justify-between [&:not(:last-child)]:border-b border-gray-200">
+          <div className="flex items-center">
+            <Checkbox className="data-[state=checked]:bg-black data-[state=checked]:text-white size-[20px] mr-4"/>
+            <div>
+              Title of the post
+              <div className="flex items-center text-gray-400 text-xs">
+                <span className="text-[#7979FF] mr-1">IT/NextJS</span>
+                <LuDot className="mr-1"/>
+                <span>2025-04-05 22:18</span>
+              </div>
+            </div>
+          </div>
+          <div className="sm:justify-self-end group-hover:flex group-hover:visible">
+            <div className="hidden lg:flex gap-2">
+              <Button variant={"destructive"}>edit</Button>
+              <Button variant={"outline"}>delete</Button>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild className="border-[#d2d2d2]">
+                  <Button variant="outline" className="text-gray-600">Public</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="min-w-[80px] border-[#d2d2d2] bg-white text-gray-600" side="right">
+                  <DropdownMenuItem
+                    className="hover:bg-gray-100"
+                  >
+                    Publice
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="hover:bg-gray-100"
+                  >
+                    Private
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            <div className="lg:hidden">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="ghost" className="ml-4 ">
+                    <span className="sr-only">Actions</span>
+                    ⋮
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="min-w-[120px] border-[#d2d2d2] bg-white text-gray-600">
+                  <DropdownMenuItem>
+                    edit
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    delete
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuLabel>Status</DropdownMenuLabel>
+                  <DropdownMenuItem>
+                    Public
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
                     Private
                   </DropdownMenuItem>
                 </DropdownMenuContent>
