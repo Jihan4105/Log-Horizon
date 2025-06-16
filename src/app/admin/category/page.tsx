ab0d4b9e6c30e2841dbf40210ecf5b2bf6d95630
Jihan4105 <wlgks6979@naver.com>
2025-06-16 21:38:20 +0900

    2025-06-16 Developments

--- src/app/admin/category/page.tsx
+++ src/app/admin/category/page.tsx
@@ -5,26 +5,10 @@ import {
   SimpleTreeItemWrapper,
   TreeItemComponentProps,
   TreeItems,
+  SortableTree
 } from 'dnd-kit-sortable-tree';
 import { MinimalTreeItemData } from '@/lib/types';
 
-import dynamic from "next/dynamic";
-const SortableTree = dynamic(
-  () => import("dnd-kit-sortable-tree").then(mod => mod.SortableTree),
-  { ssr: false }
-);
-
-export default function CategoryManagementPage() {
-  const [items, setItems] = useState(initialViableMinimalData);
-
-  return (
-    <SortableTree
-      items={items}
-      onItemsChanged={setItems}
-      TreeItemComponent={TreeItem}
-    />
-  );
-}
 /*
  * Here's the component that will render a single row of your tree
  */
@@ -32,33 +16,49 @@ const TreeItem = forwardRef<
   HTMLDivElement,
   TreeItemComponentProps<MinimalTreeItemData>
 >((props, ref) => {
-  const [sample, setSample] = useState('');
   return (
     <SimpleTreeItemWrapper {...props} ref={ref}>
+      {props.item.children?.length !== 0 && ( <div className='mr-1 text-xs font-semibold'>({props.item.children!.length})</div>)}
       <div>{props.item.value}</div>
-      <input
-        value={sample}
-        onChange={(e) => {
-          setSample(e.target.value);
-        }}
-      ></input>
     </SimpleTreeItemWrapper>
   );
 });
 TreeItem.displayName = "TreeItem";
+
+
+
 /*
  * Configure the tree data.
  */
 const initialViableMinimalData: TreeItems<MinimalTreeItemData> = [
   {
     id: 1,
-    value: 'Jane',
+    value: 'IT',
     children: [
-      { id: 4, value: 'John' },
-      { id: 5, value: 'Sally' },
+      { id: 4, value: 'HTML' },
+      { id: 5, value: 'SCSS' },
     ],
   },
-  { id: 2, value: 'Fred', children: [{ id: 6, value: 'Eugene' }] },
-  { id: 3, value: 'Helen' },
+  { id: 2, value: 'Writes', children: [{ id: 6, value: 'Novel' }] },
+  { id: 3, value: 'News' },
 ];
+
+export default function CategoryManagementPage() {
+  const [items, setItems] = useState(initialViableMinimalData);
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold">Category Management</h1>
+      <p className='mb-6'>You can sort your categories with Drag n Drop</p>
+      <div className='border-[#d2d2d2] p-4 border-1'>
+        <SortableTree
+          items={items}
+          onItemsChanged={setItems}
+          TreeItemComponent={TreeItem}
+        />
+      </div>
+      
+    </>
+  );
+}
 
